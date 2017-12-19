2cb47f8dc7cae8a768a9c5acda0da7db6c78dd66
mehrnoosh <mehrnoosh@MacBook-Pro-di-Tommaso-2.local>
2017-12-19 14:36:51 +0100

    day4

---
+++ day4/4.js
@@ -0,0 +1,46 @@
+var count = 0 ;
+let temp=[];
+var i= -1;
+var j=0;
+var lineReader = require('readline').createInterface({
+  input: require('fs').createReadStream('input.txt')
+})
+
+lineReader.on('line', function (line) {
+	i++;
+	calc(line);
+	i= -1;
+})
+
+function is_itterated (a, temp){
+	console.log(`computing ${a} - ${temp}`);
+	//console.log (i);
+	//var mtch = new RegExp (a);
+	//if (temp.match(mtch, 'g'))
+	if (temp.indexOf (a) > 0)
+		count++
+	console.log(count);
+	return count
+
+}
+
+const calc_itterate = (acc, item) =>{
+	i++
+	return {
+		itterated: is_itterated (temp[i], temp)
+	}
+} 
+
+function calc(line){
+	j++;
+	temp = line.split(" ");
+	itteration = temp.reduce(
+		calc_itterate,
+		{
+			started: temp[i],
+			//compared: temp
+		}
+		);
+	count = count - j;
+	console.log(count);
+};
