f6cb78dd1c41823289bc2287492be17b6b466e3b
mehrnoosh <mehrnoosh@MacBook-Pro-di-Tommaso-2.local>
2017-12-20 10:36:26 +0100

    changes on day4

--- day4/4.js
+++ day4/4.js
@@ -9,21 +9,30 @@ lineReader.on('line', function (line) {
 	calc(line);
 })
 
-const calc_itterate = (element, index,array) =>{
+function is_itterate (element, index, array){
+	if (array.includes(element)){
+		if (array.indexOf(element) != index )
+			return true;
+	}
+	else
+		return false;
+}
+
+const calc_itteration = (element, index, array) =>{
 	console.log(`computing ${element} - ${array}`);
 	index1= array.length;
 	console.log (index1);
-	if (array.includes(element)){
-		if ((array.indexOf(element)!=index))
-			index1--;
-	}
-	return (index1);
-} 
+	const non_itterate = is_itterate (element, index, array)
+	if (is_itterate === true)
+		return index1--;
+	else
+		return index1;
+}
 
 function calc(line){
 	temp = line.split(" ");
 	const itteration = temp.filter(
-		calc_itterate
+	calc_itteration
 		);
 	finalcount = index1 + finalcount
 	console.log(finalcount);
